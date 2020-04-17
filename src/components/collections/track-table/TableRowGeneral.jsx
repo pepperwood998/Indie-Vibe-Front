d3cdfec4f76e35d5c9d296cb96dfc4168a68a368
zuratama <tuandt66742@gmail.com>
2020-04-17 11:58:53 +0700

    Props guard

--- src/components/collections/track-table/TableRowGeneral.jsx
+++ src/components/collections/track-table/TableRowGeneral.jsx
@@ -5,70 +5,74 @@ import CellAction from './TableCellAction';
 import CellExtra from './TableCellExtra';
 import CellFavorite from './TableCellFavorite';
 
-function RowGeneral(props) {
-  const { item, serial } = props;
-
-  const playFromType =
-    props.playFromType === 'favorite' ? 'favorite' : 'release';
+function RowGeneral({
+  index = 0,
+  item = {},
+  playFromId = '',
+  playFromType = '',
+  inQueue
+}) {
+  playFromType = playFromType === 'favorite' ? 'favorite' : 'release';
+  const {
+    id = '',
+    title = '',
+    artists = [],
+    release = {},
+    duration,
+    relation
+  } = item;
+  const { artist = {} } = release;
 
   return (
     <div className='table-row content'>
       <CellAction
-        serial={serial + 1}
-        id={item.id}
-        playFromId={props.playFromId}
+        id={id}
+        serial={index + 1}
+        playFromId={playFromId}
         playFromType={playFromType}
-        inQueue={props.inQueue}
+        inQueue={inQueue}
       />
-      <CellFavorite
-        id={item.id}
-        index={serial}
-        relation={item.relation}
-        collectionKey='track'
-      />
+      <CellFavorite id={id} index={index} relation={relation} />
       <div className='title'>
-        <span className='ellipsis one-line'>{item.title}</span>
+        <span className='ellipsis one-line'>{title}</span>
       </div>
       <CellExtra
-        id={item.id}
+        id={id}
+        index={index}
+        relation={relation}
+        artistId={artist.id}
+        releaseId={release.id}
         fromType='release'
-        releaseId={item.release.id}
-        artistId={item.release.artist ? item.release.artist.id : ''}
-        index={serial}
-        relation={item.relation}
-        collectionKey='track'
-        inQueue={props.inQueue}
+        inQueue={inQueue}
       />
       <div className='artist'>
         <span className='ellipsis one-line'>
-          {item.artists
-            ? item.artists
-                .map(artist => (
-                  <NavLinkUnderline
-                    href={`/player/artist/${artist.id}`}
-                    className='font-white'
-                    key={artist.id}
-                  >
-                    {artist.displayName}
-                  </NavLinkUnderline>
-                ))
-                .reduce((prev, curr) => [prev, ', ', curr])
-            : ''}
+          {artists
+            .map(artistTmp => (
+              <NavLinkUnderline
+                href={`/player/artist/${artistTmp.id}`}
+                className='font-white'
+                key={artistTmp.id}
+              >
+                {artistTmp.displayName}
+              </NavLinkUnderline>
+            ))
+            .reduce((prev, curr) => [prev, ', ', curr])}
         </span>
       </div>
       <div className='release'>
         <span className='ellipsis one-line'>
           <NavLinkUnderline
-            href={`/player/release/${item.release ? item.release.id : ''}`}
+            href={`/player/release/${release.id}`}
             className='font-white'
           >
-            {item.release ? item.release.title : ''}
+            {release.title}
           </NavLinkUnderline>
         </span>
       </div>
       <div className='duration center side'>
         <span className='ellipsis one-line'>
-          {getFormattedTime(item.duration / 1000)}
+          {getFormattedTime(duration / 1000)}
         </span>
       </div>
     </div>
